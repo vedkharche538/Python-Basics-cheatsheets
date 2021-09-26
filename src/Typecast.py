c0d71b905a2a958227246f335070b95440507fde
Vedhas <vedhaskharche@gmail.com>
2021-09-26 23:41:41 +0530

    Datatypes-Operations added

---
+++ src/Typecast.py
@@ -0,0 +1,23 @@
+# Converting string, float to int
+s = "Hello World!"  # cant not be convert
+
+f = 5.5
+print(int(f))  # 5
+
+s1 = "5"
+print(int(s1))  # 5
+
+# Converting int, string to float
+
+i = 5
+print(float(i))  # 5.0
+
+s2 = "6"
+print(float(s2))  # 6.0
+
+# Converting int, float to string
+d = 5
+print(str(d))  # 5
+
+f1 = 5.7
+print(str(f1))  # 5.7
